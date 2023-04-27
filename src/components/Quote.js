0843249a536efa7680f6893149029aef3f7dd0ee
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-27 17:50:51 +0100

    init the quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+function QuoteComponent() {
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+  
+
+ 
+ 
+}
+
+export default QuoteComponent;
