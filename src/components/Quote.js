151d1c9ab238a668dec5bd2b2896bce5039efc8b
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-27 17:59:12 +0100

    add use effect and function to fetch data from the api

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -5,6 +5,48 @@ function QuoteComponent() {
     const [author, setAuthor] = useState("");
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchQuote = async () => {
+          try {
+            const response = await fetch(
+              "https://api.api-ninjas.com/v1/quotes?category=freedom",
+              {
+                headers: {
+                  "X-Api-Key": '5JcaEtYfVy+K33GwjVZwpQ==pHlk8DwAsjIFh954'
+                }
+              }
+            );
+            const data = await response.json();
+            const randomIndex = Math.floor(Math.random() * data.length);
+            setQuote(data[randomIndex].quote);
+            setAuthor(data[randomIndex].author);
+            setLoading(false);
+          } catch (error) {
+            setError(true);
+            setLoading(false);
+            console.error("Error fetching quote: ", error);
+          }
+        };
+    
+        fetchQuote();
+      }, []);
+
+      if (loading) {
+        return <p>Loading quote...</p>;
+      }
+    
+      if (error) {
+        return <p>Error fetching quote. Please try again later.</p>;
+      }
+    
+      return (
+        <div>
+          <p>{quote}</p>
+          <p>- {author}</p>
+        </div>
+      );
+    
   
 
  
