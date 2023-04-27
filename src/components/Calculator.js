6c925a85626ce4cfa64e66279791748941dcbfea
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-27 01:11:36 +0100

    add operate and calculate files, add a state too my functional component, and a function to make calculations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,57 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
+const Calculator = () => {
+    
+    const [state, addNewState] = useState({
+        total: null,
+        next: null,
+        operation: null,
+      });
+
+      const operation = (e) => {
+        const newState = calculate(state, e.target.textContent);
+        addNewState(newState)
+      };
+
+
+    return (
+        <div className="calculator">
     <div className="display">0</div>
     <div className="button-row">
-      <button className="button button-light" type="button">AC</button>
-      <button className="button button-light" type="button">+/-</button>
-      <button className="button button-light" type="button">%</button>
-      <button className="button button-light" type="button">&divide;</button>
+      <button className="button button-light" type="button" onClick={operation}>AC</button>
+      <button className="button button-light" type="button" onClick={operation}>+/-</button>
+      <button className="button button-light" type="button" onClick={operation}>%</button>
+      <button className="button button-light" type="button" onClick={operation}>&divide;</button>
     </div>
     <div className="button-row">
-      <button className="button" type="button">7</button>
-      <button className="button" type="button">8</button>
-      <button className="button" type="button">9</button>
-      <button className="button button-light" type="button">&times;</button>
+      <button className="button" type="button" onClick={operation}>7</button>
+      <button className="button" type="button" onClick={operation}>8</button>
+      <button className="button" type="button" onClick={operation}>9</button>
+      <button className="button button-light" type="button" onClick={operation}>&times;</button>
     </div>
     <div className="button-row">
-      <button className="button" type="button">4</button>
-      <button className="button" type="button">5</button>
-      <button className="button" type="button">6</button>
-      <button className="button button-light" type="button">-</button>
+      <button className="button" type="button" onClick={operation}>4</button>
+      <button className="button" type="button" onClick={operation}>5</button>
+      <button className="button" type="button" onClick={operation}>6</button>
+      <button className="button button-light" type="button" onClick={operation}>-</button>
     </div>
     <div className="button-row">
-      <button className="button" type="button">1</button>
-      <button className="button" type="button">2</button>
-      <button className="button" type="button">3</button>
-      <button className="button button-light" type="button">+</button>
+      <button className="button" type="button" onClick={operation}>1</button>
+      <button className="button" type="button" onClick={operation}>2</button>
+      <button className="button" type="button" onClick={operation}>3</button>
+      <button className="button button-light" type="button" onClick={operation}>+</button>
     </div>
     <div className="button-row">
-      <button className="button button-zero" type="button">0</button>
-      <button className="button" type="button">.</button>
-      <button className="button button-light" type="button">=</button>
+      <button className="button button-zero" type="button" onClick={operation}>0</button>
+      <button className="button" type="button" onClick={operation}>.</button>
+      <button className="button button-light" type="button" onClick={operation}>=</button>
     </div>
   </div>
-);
+    )
+  
+    
+};
 
 export default Calculator;
