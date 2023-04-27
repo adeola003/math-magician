9a1d1cac97f7dfb5ab065db7b9a53fb658d0ee62
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-27 18:06:16 +0100

    add styling to the quotes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import React from 'react';
 import Calculator from './components/Calculator';
+import QuoteComponent from './components/Quote';
 
 
 class App extends React.Component {
@@ -13,6 +14,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-body" id="App-body">
+          <QuoteComponent />
           <Calculator />
         </div>
       </div>
