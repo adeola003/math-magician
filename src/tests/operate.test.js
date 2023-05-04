dfee0c0b5f971fdfa628bdd8e0dfa045d6fb3534
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-04 14:53:30 +0100

    fix linters

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,25 +1,24 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
 describe('operate', () => {
-    test('it sums two numbers', () => {
-        const result = operate(2, 4, '+');
-        expect(result).toBe('6');
-    });
-    test('it substract two numbers', () => {
-        const result = operate(8, 4, '-');
-        expect(result).toBe('4');
-    });
-    test('it multiplies two numbers', () => {
-        const result = operate(2, 4, 'x');
-        expect(result).toBe('8');
-    });
-    test('it divides two numbers', () => {
-        const result = operate(8, 4, '÷');
-        expect(result).toBe('2');
-    });
-    test('it return rest from a division of two numbers', () => {
-        const result = operate(4, 2, '%');
-        expect(result).toBe('0');
-    });
+  test('it sums two numbers', () => {
+    const result = operate(2, 4, '+');
+    expect(result).toBe('6');
   });
-  
+  test('it substract two numbers', () => {
+    const result = operate(8, 4, '-');
+    expect(result).toBe('4');
+  });
+  test('it multiplies two numbers', () => {
+    const result = operate(2, 4, 'x');
+    expect(result).toBe('8');
+  });
+  test('it divides two numbers', () => {
+    const result = operate(8, 4, '÷');
+    expect(result).toBe('2');
+  });
+  test('it return rest from a division of two numbers', () => {
+    const result = operate(4, 2, '%');
+    expect(result).toBe('0');
+  });
+});
